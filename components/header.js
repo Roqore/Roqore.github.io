c61e2e007a590a30b140852c991affef8cc7c0cb
Sahan Kodituwakku <sahan.crkodituwakku@gmail.com>
2025-05-30 15:38:20 +0530

    Added template layout for the landing page

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ class RQHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header>
-        <h1>My Site</h1>
+        <h1>Roqore</h1>
         <nav>
           <a href="/">Home</a>
           <a href="/pages/about.html">About</a>
