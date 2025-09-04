f0b730f0628fdac69d20a7e56da78bb48b9b8ab9
Sahan Kodituwakku <sahan.crkodituwakku@gmail.com>
2025-09-04 09:08:12 +0530

    feat: images for service cards

--- src/components/about.ts
+++ src/components/about.ts
@@ -4,7 +4,7 @@ export function About() {
       <h2>About Us</h2>
       <p>At Roqore, we solve technology problems at their <strong>raw core</strong> — from software to data, AI, and cloud. <br>
     Our mission is simple: build practical, scalable, and future-ready solutions <br>that help businesses move faster and smarter. 
-    <a href="/about">Learn more →</a></p>
+    <a href="/about/">Learn more →</a></p>
     </section>
   `;
 }
